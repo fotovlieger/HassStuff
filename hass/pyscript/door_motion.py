42f47bdbf8fad60e27ec5eb214ea7a6cc635c3da
Hans Elbers <hans@fotovlieger.nl>
2025-01-24 17:35:25 +0100

    added door motion sensor triggering led strip

---
+++ hass/pyscript/door_motion.py
@@ -0,0 +1,20 @@
+import datetime
+def todayAt (hr, min=0, sec=0, micros=0):
+  now = datetime.datetime.now()
+  return now.replace(hour=hr, minute=min, second=sec, microsecond=micros)    
+
+@state_trigger("binary_sensor.motion1_occupancy", state_hold=0, state_check_now=True)
+def door_motion():
+  strip = "light.door_led_strip"
+  if binary_sensor.motion1_occupancy == 'on':
+    now = datetime.datetime.now()
+    if now>todayAt(17) and now<todayAt(23):
+      # Day
+      light.turn_on(entity_id=strip, rgb_color=[255,190, 110], brightness=255)
+    else:
+      # Night
+      light.turn_on(entity_id=strip, rgb_color=[255,190, 110], brightness=160)
+  else:
+    light.turn_off(entity_id=strip)
+
+log.info("doormotion")
