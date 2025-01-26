8771d288c6b71947c8e86e7df7c089c5c8a08237
Hans Elbers <hans@fotovlieger.nl>
2025-01-26 18:34:25 +0100

    added 2nd led strip

--- hass/pyscript/door_motion.py
+++ hass/pyscript/door_motion.py
@@ -1,20 +1,23 @@
 import datetime
 def todayAt (hr, min=0, sec=0, micros=0):
-  now = datetime.datetime.now()
-  return now.replace(hour=hr, minute=min, second=sec, microsecond=micros)    
+    now = datetime.datetime.now()
+    return now.replace(hour=hr, minute=min, second=sec, microsecond=micros)    
 
 @state_trigger("binary_sensor.motion1_occupancy", state_hold=0, state_check_now=True)
 def door_motion():
-  strip = "light.door_led_strip"
-  if binary_sensor.motion1_occupancy == 'on':
-    now = datetime.datetime.now()
-    if now>todayAt(17) and now<todayAt(23):
-      # Day
-      light.turn_on(entity_id=strip, rgb_color=[255,190, 110], brightness=255)
+    strip1 = "light.door_led_strip"
+    strip2 = "light.cv_led_strip"
+    if binary_sensor.motion1_occupancy == 'on':
+        now = datetime.datetime.now()
+        if now>todayAt(7) and now<todayAt(23):
+        # Day
+            light.turn_on(entity_id=strip1, rgb_color=[255,190, 110], brightness=255)
+            light.turn_on(entity_id=strip2, rgb_color=[255,190, 110], brightness=255)
+        else:
+          # Night
+            light.turn_on(entity_id=strip1, rgb_color=[255,190, 110], brightness=160)
+            light.turn_on(entity_id=strip2, rgb_color=[255,190, 110], brightness=160)
     else:
-      # Night
-      light.turn_on(entity_id=strip, rgb_color=[255,190, 110], brightness=160)
-  else:
-    light.turn_off(entity_id=strip)
+        light.turn_off(entity_id=strip)
 
 log.info("doormotion")
